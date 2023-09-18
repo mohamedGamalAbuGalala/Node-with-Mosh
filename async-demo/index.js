def71b1779b7b70e75e1462718bbf9b44a2920a2
Gasana Manzi David <GMDAVID59@GMAIL.COM>
2023-09-19 00:27:31 +0200

    Update index.js

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,32 +1,3 @@
-// const getUser = id => {     setTimeout(() => {       console.log("Reading a
-// user from database...");       return { id: id, giHubUsername: "galala" }; },
-// 2000);   }; // Callbacks // Promises // Async/await console.log("Before");
-// const user = getUser(1); console.log(user);  // undefined
-// console.log("After"); Callback ------------------------------------------
-// Start const getUser = (id, callback) => {   setTimeout(() => {
-// console.log("Reading a user from database...");     callback({ id: id,
-// giHubUsername: "galala" });   }, 2000); }; const getRepositories = (username,
-// callback) => {   setTimeout(() => { console.log("Calling GitHub API...");
-// callback(["repo1", "repo2", "repo3"]);   }, 2000); }; console.log("Before");
-// getUser(1, user => { console.log("User: ", user);
-// getRepositories(user.username, repos => console.log("Repos: ", repos)); });
-// console.log("After"); Callback ------------------------------------------ End
-// Promises ------------------------------------------ Start const getUser = id
-// => {   return new Promise((resolve, reject) => {     setTimeout(() => {
-// console.log("Reading a user from database...");       resolve({ id: id,
-// giHubUsername: "galala" });     }, 2000);   }); }; const getRepositories =
-// username => {   return new Promise((resolve, reject) => {     setTimeout(()
-// => {       console.log("Calling GitHub API...");       resolve(["repo1",
-// "repo2", "repo3"]);     }, 2000);   }); }; const getCommits = repo => {
-// return new Promise((resolve, reject) => {     setTimeout(() => {
-// console.log("Calling GitHub API...");       resolve(["commit"]);     },
-// 2000);   }); }; console.log("Before"); getUser(1)   .then(user =>
-// getRepositories(user.giHubUsername))   .then(repos => getCommits(repos[0]))
-// .then(commits => console.log("Commits: ", commits))   .catch(err =>
-// console.log("Error: ", err.message)); console.log("After"); Promises
-// ------------------------------------------ End Async and Await
-// ------------------------------------------ Start
-
 const getUser = id => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
